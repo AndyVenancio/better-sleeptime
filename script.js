7992fea0d6eadfdddc0c6a23a807721dfa49b8ac
AndyVenancio <andyv0622@gmail.com>
2022-09-07 01:00:29 -0400

    Created Rock, Paper, Scissors! game

--- script.js
+++ script.js
@@ -14,12 +14,19 @@ function handleOnClick() {
     let hours = document.getElementById("hours");
     hours.innerHTML = "";
 
+    let hh = document.getElementById("hh").value;
+    let mm = document.getElementById("mm").value;
+    let ampm = document.getElementById("ampm").value;
+    hh = ampm == "PM" ? Number.parseInt(hh) + 12 : hh;
+    console.log({hh, mm, ampm});
+
     let now = new Date()
 
     //14 minutes to fall asleep
-    now.setMinutes(now.getMinutes() + 14);
+    now.setHours(hh);
+    now.setMinutes(mm);
 
-    for (let i = 1; i < 6; i++) {
+    for (let i = 0; i < 6; i++) {
     now.setMinutes(now.getMinutes() + 90);
     let elm = document.createElement("div");
     elm.innerText =
